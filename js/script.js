80ba1a1118050987a94b9b4c3f63ad540f4098e8
Nathalia <nathalia.desenv@gmail.com>
2023-02-10 15:04:11 -0300

    Add new project
    Add new project.
    
    Signed-off-by: Nathalia <nathalia.desenv@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    circle(0, 0, 50);
+}
+
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    circle(xBolinha, yBolinha, diametro);
+}
